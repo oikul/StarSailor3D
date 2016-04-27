a676ba8c2ba5ce7ec5377093565ad14157735dc5
Lukio <Lukio@10.85.197.24>
2016-04-27 23:49:00 +0100

    initial commit

---
+++ StarSailor3D/src/renderEngine/DisplayManager.java
@@ -0,0 +1,38 @@
+package renderEngine;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
+
+public class DisplayManager {
+	
+	private static final int WIDTH = 400, HEIGHT = 300, FPS = 120;
+	
+	public static void createDisplay(){
+		ContextAttribs attribs = new ContextAttribs(3,2);
+		attribs.withForwardCompatible(true);
+		attribs.withProfileCore(true);
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.create(new PixelFormat(), attribs);
+			Display.setTitle("Star Sailor");
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+	}
+	
+	public static void updateDisplay(){
+		Display.sync(FPS);
+		Display.update();
+		
+	}
+	
+	public static void closeDisplay(){
+		Display.destroy();
+	}
+
+}
